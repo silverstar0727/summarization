a11ab136e9ea4c6c2b12b2c3e64c21e6d5ba999a
JeongMin Do <49096513+silverstar0727@users.noreply.github.com>
2021-08-21 23:23:32 +0900

    Update summarization.py

--- summarization.py
+++ summarization.py
@@ -2,15 +2,22 @@ from transformers import pipeline
 from preprocess import *
 
 def predict(url, max_length=120, min_length=30):
+    # todo: 한국어 기능 추가
     title, article = text_extraction(url)
     sentences = text_preprocessing(article)
     chunks = make_chunks(sentences)
 
+    # todo: 모델 저장하고 다시 불러오는 코드로 실행시간 단축하기
     summarizer = pipeline("summarization")
     pred = summarizer(chunks, max_length=120, min_length=30, do_sample=False)
-    print(f"results: {pred}")
 
-    return pred
+    summary = " "
+    for sub_text in pred:
+        summary += sub_text['summary_text']    
+
+    print(f"results: {summary}")
+
+    return summary
 
 if __name__ == "__main__":    
     import argparse
@@ -20,4 +27,7 @@ if __name__ == "__main__":
     parser.add_argument("--url", help="blog url")
     args = parser.parse_args()
     
-    pred = predict(args.url)
+    summary = predict(args.url)
+    title, article = text_extraction(args.url)
+    print(f"before summarization: {len(summary)}")
+    print(f"after summarization: {len(article)}")
