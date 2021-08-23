62fae23ce436cce4aa0deb8244b0ec5a35cd606b
JeongMin Do <49096513+silverstar0727@users.noreply.github.com>
2021-08-24 00:06:44 +0900

    Update get_bookmarks.py

--- get_bookmarks.py
+++ get_bookmarks.py
@@ -7,18 +7,20 @@ def add_prediction(summarizer, children):
         # type이 url일때는 summarization하여 "summary"를 key로 하는 예측 추가
         if children[i]["type"] == "url":
             children[i]["summary"] = summarizer.predict(children[i]["url"])
-            print(f"{children[i]}: {children[i]['summary']}")
+            print(children[i]["url"])
+            print(children[i]['summary'])
+
             if children[i]["summary"] == "":
                 continue
 
         # type이 folder일때는 재귀로 하위 항목을 모두 에측하고 그 결과를 children의 i번째에 저장
         else:
             children[i] = add_prediction(summarizer, children[i]["children"])
-            print(f"{children[i]}: is folder")
+            print(f"is folder")
 
     return children
 
-def get_result(summarizer, bookmarks_path="./local/bookmarks.json"):
+def get_result(summarizer, bookmarks_path="./summarization/local/bookmarks.json"):
     with open(bookmarks_path, 'r') as f:
         result = json.load(f)
     for i in range(len(result)):
@@ -26,19 +28,12 @@ def get_result(summarizer, bookmarks_path="./local/bookmarks.json"):
 
     return result
 
-def result_2_json(result, result_path="./"):
+def result_2_json(result, result_path="./result.json"):
     with open(result_path, "w") as f:
         json.dump(result, f)
 
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("--bookmarks_path", help="bookmarks json path")
-    args = parser.parse_args()
-    
     summarizer = Summarizer("t5-large")
     summarizer.get_model()
-    result = get_result(summarizer, args.bookmarks_path)
-    result_2_json()
+    result = get_result(summarizer)
+    result_2_json(result)
