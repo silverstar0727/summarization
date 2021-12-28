be78b33e2132eb77256db168a02580f33191e32c
jeongmin do <dojm0727@gmail.com>
2021-12-28 09:57:26 +0900

    코드정리

--- tfdv/validation.py
+++ tfdv/validation.py
@@ -1,10 +1,25 @@
 import tensorflow_data_validation as tfdv
 
-train_stats = tfdv.generate_statistics_from_csv(data_location='/content/sample_data/california_housing_train.csv')
-eval_stats = tfdv.generate_statistics_from_csv(data_location='/content/sample_data/california_housing_test.csv')
+def validate(train_path, eval_path):
+    train_stats = tfdv.generate_statistics_from_csv(data_location=train_path)
+    eval_stats = tfdv.generate_statistics_from_csv(data_location=eval_path)
 
-schema = tfdv.infer_schema(statistics = train_stats)
-anomalies = tfdv.validate_statistics(statistics=eval_stats, schema=schema)
+    schema = tfdv.infer_schema(statistics = train_stats)
+    anomalies = tfdv.validate_statistics(statistics=eval_stats, schema=schema)
 
-if anomalies != None:
-    raise TypeError()
+    return anomalies
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--train_path")
+    parser.add_argument("--eval_path")
+    args = parser.parse_args()
+
+
+    anomalies = validate(args.train_path, args.eval_path)
+
+    if anomalies != None:
+        raise TypeError()
